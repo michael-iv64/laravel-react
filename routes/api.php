2ef11382fa0419fa6c32e84d7447361cd5712724
Михаил Етчо <michael640@yandex.ru>
2022-11-04 21:34:17 +0300

    backend laravel

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SimpleController;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+    Route::get('me', 'me');
+    Route::get('getAll', [SimpleController::class,'getAll']);
+    // Route::get('/users', function() {
+    //     return User::all();
+    // });
+
+});
